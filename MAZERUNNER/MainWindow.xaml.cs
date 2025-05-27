01e443ed6fd4db6d4074fddcd3b510162d2c2c2f
Leonidas300-1 <leon.schoberberger@htlwy.at>
2025-05-27 14:03:13 +0200

    hallo

---
+++ MAZERUNNER/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+
+namespace WpfExample
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+<<<<<<< HEAD
+       
+        public void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            int i;
+            for (i = 0; i <= 10; i++)
+            {
+                Thread.Sleep(500);
+                Button3.Content = "Frag nicht was für Saft, einfach Orangensaft";
+
+                
+            }
+            
+            
+=======
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+
+>>>>>>> 8e2e563d0a648e4032743d8c76d710b958d7eb75
+        }
+    }
+}
